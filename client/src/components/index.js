0f4cc6be9e1749cc416395422681881c043a8030
sumitchoudharySEM <sumitsem2004@gmail.com>
2024-06-17 12:31:10 +0530

    like post function done and working

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -3,7 +3,6 @@ import Sidebar from './sidebar';
 import RightSidebar from './rightSideBar'
 import CreatePost from './CreatePost'
 import Featured from './featured';
-import PostCard from './postCard'
 
 
-export { Sidebar,RightSidebar, CreatePost, Featured, PostCard};
+export { Sidebar,RightSidebar, CreatePost, Featured};
