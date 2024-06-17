cf085b28217dca930dd04a7cef54aa6818743a6b
sumitchoudharySEM <sumitsem2004@gmail.com>
2024-06-17 09:49:53 +0530

    Follow unFollow Acoounts is built on whome to follow section

--- client/src/components/rightsidebar_compnents/ShortProfile.js
+++ client/src/components/rightsidebar_compnents/ShortProfile.js
@@ -14,9 +14,10 @@ import { Aptos } from "@aptos-labs/ts-sdk";
 const aptos = new Aptos();
 
 const ShortProfile = (props) => {
+  const { signAndSubmitTransaction } = useWallet();
   const [isProfile, setIsProfile] = useState(false);
   const [isFollowing, setIsFollowing] = useState(false);
-  
+
   const [user, setUser] = useState({
     name: "",
     username: "",
@@ -56,107 +57,85 @@ const ShortProfile = (props) => {
   useEffect(() => {
     if (isProfile == true) {
       let adminFollowing = props.userProfile.following;
-      let isAdminFollowing = adminFollowing.includes(
-        props.data
-      );
+      let isAdminFollowing = adminFollowing.includes(props.data);
       setIsFollowing(isAdminFollowing);
     }
   }, [isProfile]);
 
   return (
     <>
-    <div className=" py-2 flex space-x-4 align-middle justify-between ">
-    <Link href={`/profile/${user.profile_object_id}`} passHref>
-      <div className="flex space-x-4 align-middle">
-        <div className="">
-          <img
-            className="rounded-full h-12 w-12"
-            src={`${NEXT_PUBLIC_GATEWAY_URL}/ipfs/${user.image_cid}`}
-            alt=""
-          />
-        </div>
-        <div className="flex flex-col text-white justify-center">
-          <div className="text-lg font-medium text-[#F3F3F3] text-opacity-90">
-            {user.name}
+      <div className=" py-2 flex space-x-4 align-middle justify-between ">
+        <Link href={`/profile/${user.profile_object_id}`} passHref>
+          <div className="flex space-x-4 align-middle">
+            <div className="">
+              <img
+                className="rounded-full h-12 w-12"
+                src={`${NEXT_PUBLIC_GATEWAY_URL}/ipfs/${user.image_cid}`}
+                alt=""
+              />
+            </div>
+            <div className="flex flex-col text-white justify-center">
+              <div className="text-lg font-medium text-[#F3F3F3] text-opacity-90">
+                {user.name}
+              </div>
+              <div className="text-sm font-light text-[#D9D9D9]">
+                @{user.username}
+              </div>
+            </div>
           </div>
-          <div className="text-sm font-light text-[#D9D9D9]">
-            @{user.username}
-          </div>
+        </Link>
+        <div className="flex flex-col justify-center">
+          {isFollowing ? (
+            <button
+              onClick={async () => {
+                const transaction = {
+                  data: {
+                    function: `${MODULEADDRESS}::wizz::unfollow_profile`,
+                    functionArguments: [props.data],
+                  },
+                };
+
+                const response = await signAndSubmitTransaction(transaction);
+
+                await aptos.waitForTransaction({
+                  transactionHash: response.hash,
+                });
+                toggleFollow();
+              }}
+              className=" border-[1px] py-2 border-white px-7 w-full text-sm text-white  rounded-full font-medium"
+            >
+              Unfollow
+            </button>
+          ) : (
+            <button
+              onClick={async () => {
+                const transaction = {
+                  data: {
+                    function: `${MODULEADDRESS}::wizz::follow_profile`,
+                    functionArguments: [props.data],
+                  },
+                };
+
+                const response = await signAndSubmitTransaction(transaction);
+
+                await aptos.waitForTransaction({
+                  transactionHash: response.hash,
+                });
+                toggleFollow();
+              }}
+              className="bg-[#7501E9] py-2 px-7 w-full text-sm text-white border-none rounded-full font-medium"
+            >
+              Follow
+            </button>
+          )}
         </div>
       </div>
-      </Link>
-      <div className="flex flex-col justify-center">
-        {isFollowing ? (
-          <button
-            // onClick={() => {
-            //   const txb = new TransactionBlock();
-            //   const [unfollow] = txb.moveCall({
-            //     target: `${PACKAGE_ID}::profilecontract::unfollow_profile`,
-            //     arguments: [
-            //       txb.object(props.userProfile.profile_object_id),
-            //       txb.pure.address(user.profile_object_id),
-            //     ],
-            //   });
-            //   execUnFollow(
-            //     {
-            //       transactionBlock: txb,
-            //     },
-            //     {
-            //       onError: (err) => {
-            //         toast.error(err.message);
-            //       },
-            //       onSuccess: (result) => {
-            //         toast.success(`Digest: ${result.digest}`);
-            //         console.log("result of transaction:", result);
-            //         toggleFollow();
-            //       },
-            //     }
-            //   );
-            // }}
-            className=" border-[1px] py-2 border-white px-7 w-full text-sm text-white  rounded-full font-medium"
-          >
-            Unfollow
-          </button>
-        ) : (
-          <button
-            // onClick={() => {
-            //   const txb = new TransactionBlock();
-            //   const [follow] = txb.moveCall({
-            //     target: `${PACKAGE_ID}::profilecontract::follow_profile`,
-            //     arguments: [
-            //       txb.object(props.userProfile.profile_object_id),
-            //       txb.pure.address(user.profile_object_id),
-            //     ],
-            //   });
-            //   execFollow(
-            //     {
-            //       transactionBlock: txb,
-            //     },
-            //     {
-            //       onError: (err) => {
-            //         toast.error(err.message);
-            //       },
-            //       onSuccess: (result) => {
-            //         toast.success(`Digest: ${result.digest}`);
-            //         console.log("result of transaction:", result);
-            //         toggleFollow();
-            //       },
-            //     }
-            //   );
-            // }}
-            className="bg-[#7501E9] py-2 px-7 w-full text-sm text-white border-none rounded-full font-medium"
-          >
-            Follow
-          </button>
-        )}
-      </div>
-      
-    </div>
-    {props.setbio && props.setbio == true ?
-    <div className=" text-[#D9D9D9] text-sm mb-6 pl-0">
-    {user.bio}
-  </div> : "" }
-  </>
+      {props.setbio && props.setbio == true ? (
+        <div className=" text-[#D9D9D9] text-sm mb-6 pl-0">{user.bio}</div>
+      ) : (
+        ""
+      )}
+    </>
   );
 };
 
